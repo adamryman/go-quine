3eedefb57fe9d71efec7c24be8b5e9dff8eb8f61
Adam Ryman <adamr@tyemill.com>
2016-11-23 14:56:05 -0800

    Update with newlines, as they are equal to ; in char count.

--- main.go
+++ main.go
@@ -1,1 +1,5 @@
-package main;func main(){print(p,string(96),p,string(96))};var p string=`package main;func main(){print(p,string(96),p,string(96))};var p string=`
+package main
+func main(){print(p,string(96),p,string(96))}
+var p string=`package main
+func main(){print(p,string(96),p,string(96))}
+var p string=`
