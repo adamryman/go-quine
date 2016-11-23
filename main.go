8791a149186b8c4c281b895ca3ba0a1be7396d50
Adam Ryman <adamr@tyemill.com>
2016-11-23 14:31:18 -0800

    Update to 192 char!

--- main.go
+++ main.go
@@ -1,1 +1,1 @@
-package main;func main() {print(p);print(string(34));print(p);print(string(34));print(string(59));};var p string="package main;func main() {print(p);print(string(34));print(p);print(string(34));print(string(59));};var p string=";
+package main;func main() {print(p);print(string(34));print(p);print(string(34))};var p string="package main;func main() {print(p);print(string(34));print(p);print(string(34))};var p string=";
